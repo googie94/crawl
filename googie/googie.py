e5c5c2943975b82920cd97c8636eb063f12ee981
googie <googieyu@gmail.com>
2021-04-29 18:10:10 +0900

    add

--- googie/googie.py
+++ googie/googie.py
@@ -1,11 +1,16 @@
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup as bs
 # 
+import  os, ssl 
 
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-# b = html.read()
-# print(b)
-# print(b.h1)
+if  ( not  os.environ.get ( 'PYTHONHTTPSVERIFY', '') and getattr (ssl, '_create_unverified_context', None)) : 
+	ssl._create_default_https_context =  ssl._create_unverified_context
+
+keyword = '%EA%B7%80%EB%A9%B8%EC%9D%98%20%EC%B9%BC%EB%82%A0'
+keyword.encode('utf-8')
+url = 'https://namu.wiki/w/'+keyword
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+html = urlopen(req)
 bs = bs(html.read(), 'html.parser')
 print(bs)
-print(bs.h1)
