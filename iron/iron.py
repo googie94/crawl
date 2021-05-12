d5e131f95daff7ccd14661ee251943a278451286
IRON <iron@IRONui-MacBookPro.local>
2021-05-12 11:28:18 +0900

    iron

--- iron/iron.py
+++ iron/iron.py
@@ -2,15 +2,22 @@ import time
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome("/Users/iron/Documents/iron/crawl/driver/chromedriver")
+#options = Options()
+#options.add_argument("--headless")
+#options.add_argument("window-size=1920,1080")
+
+driver = webdriver.Chrome("/Users/iron/Documents/iron/crawl/driver/chromedriver") #,options=options
 driver.implicitly_wait(3)
 
 driver.get('https://www.instagram.com/')
 time.sleep(1)
 
 driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys('footballtobe11@gmail.com')
+time.sleep(1)
 driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys('plab0104!')
+time.sleep(1)
 driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]').click()
 time.sleep(1)
 
@@ -29,6 +36,9 @@ while x < 9:
         post_selector = '#react-root > section > main > article > div:nth-child(3) > div > div:nth-child({}) > div:nth-child({}) > a'.format(x, y)
         post_url = driver.find_element_by_css_selector(post_selector).get_attribute('href')
         post_urls.append(post_url)
+    time.sleep(1)
     x += 1
+
+print(post_urls)
 
 driver.quit()
