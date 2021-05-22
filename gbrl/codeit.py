fba0490d495561335bb056dea25ab28d14e23eee
drilljin <jinhocheol@naver.com>
2021-05-22 21:19:05 +0900

    2

--- gbrl/codeit.py
+++ gbrl/codeit.py
@@ -1,49 +1,50 @@
-import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from bs4 import BeautifulSoup
+import pandas as pd
 
-# 웹 드라이버 설정
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("C:/Users/SAMSUNG/Downloads/chromedriver_win32/chromedriver.exe") 
+
+driver.get("https://cafe.naver.com/re4mo")    
 driver.implicitly_wait(3)
 
-driver.get('https://workey.codeit.kr/costagram/index')
-time.sleep(1)
+driver.find_element_by_name('query').send_keys('플랩풋볼')  
+driver.find_element_by_name("query").send_keys(Keys.ENTER)  # 엔터를 입력해 검색
+time.sleep(2)  # 2초 대기
 
-# 로그인
-driver.find_element_by_css_selector('.top-nav__login-link').click()
-time.sleep(1)
+driver.switch_to.frame("cafe_main")
 
-driver.find_element_by_css_selector('.login-container__login-input').send_keys('codeit')
-driver.find_element_by_css_selector('.login-container__password-input').send_keys('datascience')
+for i in range(1, 5):
+    req = driver.page_source
+    soup = BeautifulSoup(req, 'html.parser')
+    titles = soup.select("#main-area > div:nth-child(7) > table > tbody > tr")
 
-driver.find_element_by_css_selector('.login-container__login-button').click()
-time.sleep(1)
+#main-area > div.article-board.result-board.m-tcol-c > table > tbody > tr:nth-child(2) 
+#> td.td_article > div.board-list > div > a.article
 
-# 페이지 끝까지 스크롤
-last_height = driver.execute_script("return document.body.scrollHeight")
+#app > div > div.container > div > div.SectionSearchContent > div.section_search_content > div > div.article_list_area > ul 
+# > li:nth-child(1) > div > div > div > a
+    print('----' + str(i) + ' 번째 페이지 -----')
+    list3 = []
 
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    for title in titles:
+        list = title.select_one(' td.td_article > div.board-list > div > a').text
+        list2 = ''.join(list.split())
+        list3.append(list2)
 
-    time.sleep(0.5)
+    list4_sr = pd.Series(list3)
+    print(list4_sr)
 
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+    # for a in range(1, 3):
+        # driver.find_element_by_xpath(f'//*[@id="main-area"]/div[5]/table/tbody/tr[{a}]/td[1]/div[2]/div/a').click()
+        # time.sleep(3)
+        # driver.back()
+        # time.sleep(2)
+        # driver.switch_to.frame("cafe_main")
+        ## 게시글을 클릭하고 뒤로 돌아온 경우에는 switch로 다시 iframe을 선택해 주어야 한다.
+    if i<4:
+        driver.find_element_by_xpath(f'//*[@id="main-area"]/div[7]/a[{i}+1]').click()
+        # 결과 다음 페이지로 가는 구문, 다음페이지로 간 경우 iframe 은 선택 되어 있으므로
+        # 스위치를 안써줘도 된다.
 
-# 모든 썸네일 요소 가져오기
-posts = driver.find_elements_by_css_selector('.post-list__post')
-
-for post in posts:
-    # 썸네일 클릭
-    post.click()
-    time.sleep(0.5)
-
-    ### 코드를 작성하세요 ###
-    driver.find_element_by_css_selector('.web_element.get_attribute("/images/costagram/posts/post1.jpg").split()')
-
-    # 닫기 버튼 클릭
-    driver.find_element_by_css_selector('.close-btn').click()
-    time.sleep(0.5)
-
-driver.quit()
--- gbrl/naver_cafe.py
+++ gbrl/naver_cafe.py
@@ -1,61 +1,22 @@
-from selenium import webdriver
 import time
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import pyperclip
+from selenium import webdriver
 
-def clipboard_input(user_xpath, user_input):
-        temp_user_input = pyperclip.paste()  # 사용자 클립보드를 따로 저장
-
-        pyperclip.copy(user_input)
-        driver.find_element_by_xpath(user_xpath).click()
-        ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
-
-        pyperclip.copy(temp_user_input)  # 사용자 클립보드에 저장 된 내용을 다시 가져 옴
-        time.sleep(1)
-
-driver = webdriver.Chrome("C:/Users/SAMSUNG/Downloads/chromedriver_win32/chromedriver.exe")
+driver = webdriver.Chrome('C:/Users/SAMSUNG/Downloads/chromedriver_win32/chromedriver.exe')
 driver.implicitly_wait(3)
 
-# 접속할 url
-url = "https://cafe.naver.com/re4mo/1513599"
-
-# 접속 시도
-driver.get(url)
-
-login = {
-    "id" : "tigerz0114",
-    "pw" : "wlsghcjf!1"
-}
-
-clipboard_input('//*[@id="id"]', login.get("id"))
-clipboard_input('//*[@id="pw"]', login.get("pw"))
-driver.find_element_by_xpath('//*[@id="log.login"]').click()
-
-# 코드잇 네이버 블로그 접속
+driver.get('https://m.cafe.naver.com/ca-fe/home/search/articles?q=%ED%94%8C%EB%9E%A9%ED%92%8B%EB%B3%BC')
 time.sleep(1)
 
-driver.find_element_by_css_selector('input.#query').click()
+driver.find_element_by_xpath('//*[@id="ct"]/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/a/div[1]/strong').click()
 
-driver.find_element_by_css_selector().send_keys('플랩풋볼')
+print(driver.find_element_by_xpath('//*[@id="ct"]/div[1]/div/h2').text)
+print(driver.find_element_by_xpath('//*[@id="postContent"]/div/div/div').text)
 
-driver.find_element_by_css_selector('ico_search_submit').click()
-
-
-# 'mainFrame'으로 이동
-driver.switch_to.frame('cafe_main')
-
-# 블로그 글 내용 출력
-print(driver.find_element_by_css_selector('div.se-main-container').text)
+for in :
+	
+	driver.find_element_by_xpath('').click()
+	print(driver.find_element_by_xpath('//*[@id="ct"]/div[1]/div/h2').text)
+	print(driver.find_element_by_xpath('//*[@id="postContent"]/div/div/div').text)
+	driver.back()
 
 driver.quit()
-
-#현 상황 요약 , 네이버 블로그 iframe 제목, 기사 돌파 성공 -> 카페 시도 => 
-#캡챠 갑툭튀 => 디버깅 크롬 뭐시기로 돌파중 ...
-
-#driver.switch_to.default_content() #처음 상태로 되돌아옴
-
-#git add --all
-#git status 
-#git commit -am ""
-#git push origin master 
