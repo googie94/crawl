84a942792f08b2426861d9b440d7b54fa85bc018
googie <googieyu@gmail.com>
2021-05-23 19:35:22 +0900

    add

--- gbrl/naver_cafe.py
+++ gbrl/naver_cafe.py
@@ -1,29 +1,32 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:/Users/SAMSUNG/Downloads/chromedriver_win32/chromedriver.exe')
+driver = webdriver.Chrome()
 driver.implicitly_wait(3)
 
+# 들어가는거
 driver.get('https://m.cafe.naver.com/ca-fe/home/search/articles?q=%ED%94%8C%EB%9E%A9%ED%92%8B%EB%B3%BC')
 time.sleep(1)
 
-driver.find_element_by_xpath('//*[@id="ct"]/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/a/div[1]/strong').click()
+# driver.back()
 
-print(driver.find_element_by_xpath('//*[@id="ct"]/div[1]/div/h2').text)
-print(driver.find_element_by_xpath('//*[@id="postContent"]/div/div/div').text)
-
-driver.back()
-
-
-
-for i in range(1,5):
-	print(i)
-	click = driver.find_element_by_css_selector('#ct > div > div > div > div:nth-child(2) > div > div > div > div.SearchResultsListItemContainer.list_item_container.article_list > div:nth-child({}) > strong').format(i)
-	click.click()
+for index in range(1,480):
+	time.sleep(3)
+	print(index)
+	print(type(index))
+	index = str(index)
+	print(type(index))
+	# 클릭한다
+	a = driver.find_element_by_xpath("//*[@id='ct']/div/div/div/div[2]/div/div/div/div[2]/div["+index+"]/div/a/div[1]/strong")
+	a.click()
+	time.sleep(3)
+	# 가져온다
 	print(driver.find_element_by_xpath('//*[@id="ct"]/div[1]/div/h2').text)
 	print(driver.find_element_by_xpath('//*[@id="postContent"]/div/div/div').text)
+	time.sleep(3)
+	# 나간다
 	driver.back()
-
+	time.sleep(3)
 
 driver.quit()
 
